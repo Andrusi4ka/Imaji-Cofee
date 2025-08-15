2b32be7ab3bd682df6ed71da9377da72a39e83e5
Andrusi4ka <Andrusi4ka1985@gmail.com>
2025-08-16 00:48:25 +0200

    Добавлено Javascript

--- js/script.js
+++ js/script.js
@@ -11,18 +11,6 @@ if (document.querySelector("#menu-bar")) {
 }
 
 
-/***************** PRODUCT-PAGE / small product photos *******************/
-if (document.querySelector("#product-page")) {
-    let xsImg = Array.from(document.querySelectorAll("#product-page .thumbnail-gallery img"));
-    const xlImg = document.querySelector("#product-page .main-image");
-
-    for (let i = 0; i < xsImg.length; i++) {
-        xsImg[i].onclick = function () {
-            xlImg.innerHTML = xsImg[i].outerHTML;
-        }
-    }
-}
-
 /**************** читати більше в карточці товару ******************/
 if (document.querySelector("#product-page")) {
     const readBtn = document.querySelector("#product-page .product-info .product-description .read-more");
@@ -51,7 +39,6 @@ if (document.querySelector("#home-page")) {
         mainApp.style.height = Math.round(appItems.offsetHeight * 0.2) + 'px';
         mainWorkspace.style.paddingTop = Math.round(appItems.offsetHeight * 0.8) + 'px';
         appItems.style.position = "absolute";
-        console.log(Math.round(appItems.offsetHeight * 0.2) + 'px');
     }
 
     window.addEventListener('resize', () => {
@@ -82,3 +69,97 @@ if (document.querySelector("#product-page")) {
         }
     });
 }
+
+/*************** HOME PAGE. swiper slider ****************/
+if (document.querySelector("#home-page")) {
+    new Swiper('.workspaces', {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+        grabCursor: true,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            768: {
+                spaceBetween: 20,
+                slidesPerView: 2.5,
+            },
+            992: {
+                spaceBetween: 30,
+                slidesPerView: 3.5,
+            },
+            1200: {
+                spaceBetween: 40,
+                slidesPerView: 4.5
+            }
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
+
+
+/********** PRODUCT-PAGE / вставляє маленьке фото у головне фото. Підставляє головне фото в модальне вікно *******/
+if (document.querySelector("#product-page")) {
+    let xsImg = Array.from(document.querySelectorAll("#product-page .thumbnail-gallery img"));
+    const xlImg = document.querySelector("#product-page .main-image");
+
+    if (document.querySelector('#product-image-modal')) {
+        const modalBody = document.querySelector('#product-image-modal .modal-body');
+        modalBody.innerHTML = xlImg.innerHTML;
+        modalBody.querySelector('img').classList.remove('w-100');
+    }
+
+    for (let i = 0; i < xsImg.length; i++) {
+        xsImg[i].onclick = function () {
+            xlImg.innerHTML = xsImg[i].outerHTML;
+
+            if (document.querySelector('#product-image-modal')) {
+                const modalBody = document.querySelector('#product-image-modal .modal-body');
+                modalBody.innerHTML = xlImg.innerHTML;
+                modalBody.querySelector('img').classList.remove('w-100');
+            }
+        }
+    }
+}
+
+
+/******************** Ховає та показує пароль ****************/
+if (document.querySelector('.eye')) {
+    const openEye = '<i class="bi bi-eye"></i>';
+    const closedEye = '<i class="bi bi-eye-slash-fill"></i>';
+    document.querySelector('.eye').addEventListener('click', function () {
+        if (this.innerHTML === openEye) {
+            this.innerHTML = closedEye;
+            this.parentElement.type = 'password';
+        } else {
+            this.innerHTML = openEye;
+            this.parentElement.type = 'text';
+        }
+    });
+}
+
+
+/*************** HOME PAGE. swiper slider ****************/
+if (document.querySelector("#home-page")) {
+
+    new Swiper('.main-locations', {
+        slidesPerView: 4,
+        effect: "fade",          // ефект плавного зникнення
+        fadeEffect: {
+            crossFade: true        // одночасне згасання/поява
+        },
+        autoplay: {
+            delay: 3000,           // затримка між слайдами (мс)
+            disableOnInteraction: false // не зупиняти після ручного свайпу
+        },
+        loop: true               // зациклення
+    });
+
+}
+
+
